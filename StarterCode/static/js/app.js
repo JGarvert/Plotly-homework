4d66d3cf742c12bcfd39b37f8f695f60ec836d81
JGarvert <julie.garvert@adidas.com>
2021-04-06 18:20:05 -0700

    working on bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,17 +4,27 @@ d3.json('samples.json').then(data => {
 })
 
 // Create a horizontal bar chart.
-// 1. Will need a trace to create the plot
+// 1. Will need a trace to create the plot. Additionally, cut to top 10.
 var trace1 = {
     x: ,
     y: ,
-    type: "bar"
+    type: "bar",
+    orientation: "h",
+    text: otu_labels,
+    transforms: [{
+        type: 'sort',
+        target: 'y',
+        order: 'descending'
+    }]
 };
-// 2. Define horizontal plot layout
-var layout = {
+// 2. Create the array for the plot
+var bar_data = [trace1];
+
+// 3. Define horizontal plot layout
+var bar_layout = {
     title: "Top 10 OTUs found in selected individual",
     xaxis: { title: "Number of occurances"},
-    yaxis: ( title: "OTU ID")
+    yaxis: { title: "OTU ID"}
 };
-// Plot the chart to a div tag
-Plotly.newPlot("bar-plot", data, layout);
+// 4. Plot the chart to a div tag
+Plotly.newPlot("bar-plot", bar_data, bar_layout);
